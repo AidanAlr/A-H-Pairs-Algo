baef1bc1f2a88bd43cabd070859e2948c8dd13b7
AidanAlr <aidanalrawi@icloud.com>
2022-11-10 15:31:54 +0000

    H + A

--- User Inputs.py
+++ User Inputs.py
@@ -8,17 +8,44 @@ import os
 import glob
 import yfinance as yf
 
+pd.set_option('display.max_columns', None)
+
+def main():
+    stocks = stock_list()
+    extracted_data(stocks)
+    stock_df = extracted_data(stocks)
+    highest_corr_pair(stock_df)
+
+
 def stock_list():
     user_input = input('Enter your tickers separated by a "space" please.')
-    stock_l = list(user_input.upper().split())
-    return stock_l
+    stocks = list(user_input.upper().split())
+    return stocks
 
-def extract_data():
+
+def extracted_data(stocks):
     ticker_string = ''
-    for tickers in stock_list():
+    for tickers in stocks:
         ticker_string += tickers + ' '
-
     ticker_string = ticker_string.strip()
-    print(yf.download(f'{ticker_string}', start='2015-01-01', end='2020-01-01'))
 
-extract_data()
+    stock_df = yf.download(f'{ticker_string}', start='2015-01-01', end='2020-01-01')
+    stock_df = stock_df.drop(['Adj Close', 'High', 'Open', 'Low', 'Volume'], axis=1)
+    print(stock_df.index)
+
+    print('Your generated stock data: ')
+    print(stock_df)
+    return stock_df
+
+
+def highest_corr_pair(stock_df):
+    print('Correlation Matrix generated from your stock picks: ')
+    corr_matrix = stock_df.corr()
+    print(corr_matrix)
+    # corr_pairs = corr_matrix.unstack().drop_duplicates().sort_values(kind='quicksort')
+    # np.fill_diagonal(corr_matrix.values, np.nan)
+    # print(corr_matrix)
+    # print('\n')
+
+
+main()
