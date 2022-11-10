e33190b9e8c68d43253713949ed6d01d4bc05e9c
AidanAlr <aidanalrawi@icloud.com>
2022-11-10 16:05:27 +0000

    Updating Codebase

--- User Inputs.py
+++ User Inputs.py
@@ -12,7 +12,6 @@ pd.set_option('display.max_columns', None)
 
 def main():
     stocks = stock_list()
-    extracted_data(stocks)
     stock_df = extracted_data(stocks)
     highest_corr_pair(stock_df)
 
@@ -29,9 +28,7 @@ def extracted_data(stocks):
         ticker_string += tickers + ' '
     ticker_string = ticker_string.strip()
 
-    stock_df = yf.download(f'{ticker_string}', start='2015-01-01', end='2020-01-01')
-    stock_df = stock_df.drop(['Adj Close', 'High', 'Open', 'Low', 'Volume'], axis=1)
-    print(stock_df.index)
+    stock_df = yf.download(f'{ticker_string}', start='2015-01-01', end='2020-01-01')['Adj Close']
 
     print('Your generated stock data: ')
     print(stock_df)
@@ -42,7 +39,11 @@ def highest_corr_pair(stock_df):
     print('Correlation Matrix generated from your stock picks: ')
     corr_matrix = stock_df.corr()
     print(corr_matrix)
-    # corr_pairs = corr_matrix.unstack().drop_duplicates().sort_values(kind='quicksort')
+    print('''
+    Correlation Pairs:''')
+    corr_pairs = corr_matrix.unstack().drop_duplicates().sort_values(kind='quicksort')
+    np.fill_diagonal(corr_matrix.values, np.nan)
+    print(corr_pairs)
     # np.fill_diagonal(corr_matrix.values, np.nan)
     # print(corr_matrix)
     # print('\n')
